8ea3cf0ac4cc4bd5509026f20f328b52b80363f9
Zip <dzieppe2@gmail.com>
2024-07-16 12:27:28 -0300

    arquivos

---
+++ script.js
@@ -0,0 +1,11 @@
+function copyEmail() {
+    const email = 'Email:Dzieppe@gmail.com';
+    navigator.clipboard.writeText(email);
+    alert('Email copiado ');
+}
+
+function copyPhone() {
+    const tel = '5199880-5489';
+    navigator.clipboard.writeText(tel);
+    alert('Telefone copiado ');
+}
